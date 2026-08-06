a2ff9f1a352a00b1412a4997841c3ee89aeae63f
IHEBZARGOUNI <iheb.zargouni.pr@gmail.com>
2026-08-06 15:37:36 +0100

    feat(core): implement global error handling

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../shared/errors/apiError";
+
+export function errorHandler(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+
+    if (error instanceof ApiError) {
+
+        return res.status(error.statusCode).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
+    console.error(error);
+
+    return res.status(500).json({
+
+        success: false,
+
+        message: "Internal Server Error"
+
+    });
+
+}
